4125e9f818df21f61e35125f8766269fabd0157c
shanclient <141842941+shanclient@users.noreply.github.com>
2024-06-02 09:48:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("🎬 Main Channel", url="https://t.me/+IL4Y_AUfQ8xiNTE1")
+                    InlineKeyboardButton("🎬 Main Channel", url="https://t.me/+QKe_u9PmObZmYzc9")
                 ],
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
